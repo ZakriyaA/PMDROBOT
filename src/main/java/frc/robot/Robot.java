245d64e0c2b77d5a9ab29f9a8fa4795ed23b9c0c
IMRAN <ZAKRIYAASHRAF3@gmail.com>
2024-06-16 18:54:22 +1000

    Updated Main.java and Robot.java with new changes to shooter mechanism

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -11,31 +11,50 @@ import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 
 /**
- * This is a demo program showing the use of the DifferentialDrive class, specifically it contains
- * the code necessary to operate a robot with tank drive.
+ * This is a demo program showing the use of the DifferentialDrive class,
+ * specifically it contains the code necessary to operate a robot with tank drive.
  */
 public class Robot extends TimedRobot {
+  // Declaration of robot drive system and joystick objects
   private DifferentialDrive m_myRobot;
   private Joystick m_leftStick;
   private Joystick m_rightStick;
 
-  private final MotorController m_leftMotor = new PWMSparkMax(0);
-  private final MotorController m_rightMotor = new PWMSparkMax(1);
+  // Declaration of motor controller objects for the left and right drive motors
+  private final MotorController m_leftMotor = new PWMSparkMax(0); // Left drive motor connected to PWM port 0
+  private final MotorController m_rightMotor = new PWMSparkMax(1); // Right drive motor connected to PWM port 1
+
+  // Declaration of motor controller objects for additional motors
+  private final MotorController m_extraMotor1 = new PWMSparkMax(2); // Extra motor 1 connected to PWM port 2
+  private final MotorController m_extraMotor2 = new PWMSparkMax(3); // Extra motor 2 connected to PWM port 3
 
   @Override
   public void robotInit() {
-    // We need to invert one side of the drivetrain so that positive voltages
-    // result in both sides moving forward. Depending on how your robot's
-    // gearbox is constructed, you might have to invert the left side instead.
+    // Invert the right motor to ensure both motors move the robot forward
     m_rightMotor.setInverted(true);
 
+    // Initialize the differential drive with the left and right motors
     m_myRobot = new DifferentialDrive(m_leftMotor, m_rightMotor);
+
+    // Initialize the joysticks on ports 0 and 1
     m_leftStick = new Joystick(0);
     m_rightStick = new Joystick(1);
   }
 
   @Override
   public void teleopPeriodic() {
+    // Control the robot using tank drive by getting the Y-axis values from the joysticks
     m_myRobot.tankDrive(-m_leftStick.getY(), -m_rightStick.getY());
+
+    // Check if button 1 on the left joystick is pressed
+    if (m_leftStick.getRawButton(1)) {
+      // Set the extra motors to a certain speed when the button is pressed
+      m_extraMotor1.set(0.5); // Set motor 1 to 50% speed
+      m_extraMotor2.set(0.5); // Set motor 2 to 50% speed
+    } else {
+      // Stop the extra motors when the button is not pressed
+      m_extraMotor1.stopMotor();
+      m_extraMotor2.stopMotor();
+    }
   }
 }
